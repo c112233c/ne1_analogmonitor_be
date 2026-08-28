b3397ea29dc1984dc4896954f498ac873a623c2d
Boat <r.wongsaphan@gmail.com>
2026-08-28 14:35:09 +0700

    try

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const requireKeycloakAuth = require("./middleware/keycloak");
 const openApiSpec = yaml.load(
     fs.readFileSync(path.join(__dirname, "../openapi.yaml"), "utf8")
 );
+console.log();
 const app = express();
 app.use(cors());
 app.use(express.json());
